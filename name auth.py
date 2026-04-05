321fb1211b03a5b8585e3c14852a0455469011df
7rupai-tech <7rupaidogla@gmail.com>
2026-04-06 00:08:13 +0530

    Add login functionality with user authentication
    Implement a simple login system using Streamlit.

--- name auth.py
+++ name auth.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+USERS = {
+    "admin": "1234",
+    "aditya": "1234"
+}
+
+def login():
+    st.title("🔐 Login")
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        if USERS.get(username) == password:
+            st.session_state["logged_in"] = True
+            st.success("Login successful")
+        else:
+            st.error("Invalid credentials")
